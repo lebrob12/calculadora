562bb36fe03240b5f844e1b46275d17d3b5e5dba
lebrob12 <69060598+lebrob12@users.noreply.github.com>
2020-12-26 13:38:27 +0000

    Calculator 1.04V
    Division by 0 Fixed
    Added del button
    Exit window added

--- calc.java
+++ calc.java
@@ -1,13 +1,13 @@
-
-
+package com.company;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.*;
 
 public class calc {
-    File file=new File("history.txt");
     private JPanel mainpanel;
     private JButton a0button;
     private JTextField txtdisplay;
@@ -28,18 +28,14 @@ public class calc {
     private JButton multibutton;
     private JButton equalbutton;
     private JButton historyButton;
-    private  double total;
+    private JButton del;
+    private double total;
     private String sy;
-     static ImageIcon logo= new ImageIcon("example.jpg");
+    static ImageIcon logo = new ImageIcon("example.jpg");
     public calc() {
-
-
-
-
-
-
-
-
+        File file = new File("history.txt");
+        txtdisplay.setEditable(false);
+        txtdisplay.setBackground(Color.white);
         historyButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -48,7 +44,7 @@ public class calc {
                     BufferedReader in = new BufferedReader(new FileReader(new File("history.txt")));
                     String line;
                     try {
-                        while((line = in.readLine())!= null){
+                        while ((line = in.readLine()) != null) {
                             text = text + line;
                         }
                         in.close();
@@ -61,24 +57,22 @@ public class calc {
                 String textToDisplay = text;
                 JPanel panel = new JPanel();
                 JLabel jlabel = new JLabel(textToDisplay);
-                jlabel.setFont(new Font("Verdana",1,10));
+                jlabel.setFont(new Font("Verdana", 1, 10));
                 panel.add(jlabel);
                 panel.setVisible(true);
             }
         });
-
-
         a1Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a1txt=txtdisplay.getText()+a1Button.getText();
+                String a1txt = txtdisplay.getText() + a1Button.getText();
                 txtdisplay.setText(a1txt);
             }
         });
         a2button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a2txt=txtdisplay.getText()+a2button.getText();
+                String a2txt = txtdisplay.getText() + a2button.getText();
                 txtdisplay.setText(a2txt);
 
 
@@ -87,7 +81,7 @@ public class calc {
         a3Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a3txt=txtdisplay.getText()+a3Button.getText();
+                String a3txt = txtdisplay.getText() + a3Button.getText();
                 txtdisplay.setText(a3txt);
 
 
@@ -96,7 +90,7 @@ public class calc {
         a4Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a4txt=txtdisplay.getText()+a4Button.getText();
+                String a4txt = txtdisplay.getText() + a4Button.getText();
                 txtdisplay.setText(a4txt);
 
 
@@ -105,7 +99,7 @@ public class calc {
         a5Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a5txt=txtdisplay.getText()+a5Button.getText();
+                String a5txt = txtdisplay.getText() + a5Button.getText();
                 txtdisplay.setText(a5txt);
 
 
@@ -114,7 +108,7 @@ public class calc {
         a6Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a6txt=txtdisplay.getText()+a6Button.getText();
+                String a6txt = txtdisplay.getText() + a6Button.getText();
                 txtdisplay.setText(a6txt);
 
 
@@ -123,7 +117,7 @@ public class calc {
         a7Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a7txt=txtdisplay.getText()+a7Button.getText();
+                String a7txt = txtdisplay.getText() + a7Button.getText();
                 txtdisplay.setText(a7txt);
 
 
@@ -132,36 +126,36 @@ public class calc {
         a8Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a8txt=txtdisplay.getText()+a8Button.getText();
+                String a8txt = txtdisplay.getText() + a8Button.getText();
                 txtdisplay.setText(a8txt);
             }
         });
         a9Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a9txt=txtdisplay.getText()+a9Button.getText();
+                String a9txt = txtdisplay.getText() + a9Button.getText();
                 txtdisplay.setText(a9txt);
             }
         });
         a0button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String a0txt=txtdisplay.getText()+a0button.getText();
+                String a0txt = txtdisplay.getText() + a0button.getText();
                 txtdisplay.setText(a0txt);
             }
         });
         pointbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String dis=txtdisplay.getText()+pointbutton.getText();
+                String dis = txtdisplay.getText() + pointbutton.getText();
                 txtdisplay.setText(dis);
             }
         });
         plusbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                total=total+Double.parseDouble(txtdisplay.getText());
-                sy=plusbutton.getText();
+                total = total + Double.parseDouble(txtdisplay.getText());
+                sy = plusbutton.getText();
                 txtdisplay.setText("");
 
             }
@@ -169,8 +163,8 @@ public class calc {
         divisonbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                total=total+Double.parseDouble(txtdisplay.getText());
-                sy=divisonbutton.getText();
+                total = total + Double.parseDouble(txtdisplay.getText());
+                sy = divisonbutton.getText();
                 txtdisplay.setText("");
 
             }
@@ -178,8 +172,8 @@ public class calc {
         multibutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                total=total+Double.parseDouble(txtdisplay.getText());
-                sy= multibutton.getText();
+                total = total + Double.parseDouble(txtdisplay.getText());
+                sy = multibutton.getText();
                 txtdisplay.setText("");
 
             }
@@ -187,8 +181,8 @@ public class calc {
         minusbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                total=total+Double.parseDouble(txtdisplay.getText());
-                sy=minusbutton.getText();
+                total = total + Double.parseDouble(txtdisplay.getText());
+                sy = minusbutton.getText();
                 txtdisplay.setText("");
 
             }
@@ -197,9 +191,9 @@ public class calc {
         clearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                total=0;
+                total = 0;
                 txtdisplay.setText("");
-                sy="";
+                sy = "";
             }
         });
         equalbutton.addActionListener(new ActionListener() {
@@ -207,46 +201,76 @@ public class calc {
             public void actionPerformed(ActionEvent e) {
                 switch (sy) {
                     case "+":
-                    total = total + Double.parseDouble(txtdisplay.getText());
-                    txtdisplay.setText(String.valueOf(total));
-                    break;
+                        total = total + Double.parseDouble(txtdisplay.getText());
+                        txtdisplay.setText(String.valueOf(total));
+                        break;
                     case "*":
                         total = total * Double.parseDouble(txtdisplay.getText());
                         txtdisplay.setText(String.valueOf(total));
                         break;
                     case "/":
-                        total = total / Double.parseDouble(txtdisplay.getText());
-                        txtdisplay.setText(String.valueOf(total));
+                        if(Double.parseDouble(txtdisplay.getText())!=0) {
+                            total = total / Double.parseDouble(txtdisplay.getText());
+                            txtdisplay.setText(String.valueOf(total));
+                        } else{
+                            txtdisplay.setText("Error:Divison by 0");
+                        }
                         break;
                     case "-":
                         total = total - Double.parseDouble(txtdisplay.getText());
                         txtdisplay.setText(String.valueOf(total));
                         break;
                 }
-               try {
-                   FileWriter in=new FileWriter(file);
+                try {
+                    FileWriter in = new FileWriter(file);
                     in.write(String.valueOf(total));
                     in.close();
 
-               }
-             catch (IOException ioException) {
-                   ioException.printStackTrace();
-               }
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
             }
         });
 
+       del.addActionListener(new ActionListener() {
+           @Override
+            public void actionPerformed(ActionEvent e) {
+               if (algarismos(Double.parseDouble(txtdisplay.getText())) == 1) {
+                   txtdisplay.setText("");
+               }else {
+                   String txt=txtdisplay.getText();
+                   txt = txt.substring(0,txt.length()-1);
+                   txtdisplay.setText(txt);
+               }
+           }
+      });
     }
-
+public static int algarismos(double num){
+    String s = "" + num;
+    String[] result = s.split("\\.");
+        return result[0].length() + result[1].length();
+}
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculadora");
         frame.setContentPane(new calc().mainpanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        ImageIcon logo= new ImageIcon("example.png");
+        ImageIcon logo = new ImageIcon("example.png");
         frame.setVisible(true);
         frame.setResizable(false);
-        frame.setSize(500,300);
+        frame.setSize(500, 300);
         frame.setIconImage(logo.getImage());
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                int result = JOptionPane.showConfirmDialog(frame,
+                        "Do you want to Exit ?", "Exit Confirmation : ",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION)
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                else if (result == JOptionPane.NO_OPTION)
+                    frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            }
+        });
     }
 
 
